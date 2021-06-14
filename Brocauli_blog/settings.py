90bec13b07e4421792c4190da618135da34967db
Pk47-iso-9001 <technicalbug47@gmail.com>
2021-06-14 22:09:26 +0545

    fifth commit

--- Brocauli_blog/settings.py
+++ Brocauli_blog/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["brocauli.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["https://brocauli.herokuapp.com"]
 
 
 # Application definition
